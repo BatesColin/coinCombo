da4da3141243d445f284ee3d87bb6fdcf58f2eaf
Colin Bates and Jean Jia <colin@>
2018-06-27 14:15:12 -0700

    Create Initial Project Folder.

---
+++ Coin-Combo.Tests/ModelsTests/Coin-ComboTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Coin-Combo;
+
+namespace Coin-Combo.Tests
+{
+  [TestClass]
+  public class Coin-ComboTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ Coin-Combo/Models/Coin-Combo.cs
@@ -0,0 +1,12 @@
+namespace Coin-Combo
+{
+public class Coin-Combo
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
+  
