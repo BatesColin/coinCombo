a3bea051b30190e0fea52eb827721151d5b01f05
Colin Bates and Jean Jia <colin@>
2018-06-27 15:03:21 -0700

    working on method.

--- Coin-Combo/Models/Coin-Combo.cs
+++ Coin-Combo/Models/Coin-Combo.cs
@@ -1,12 +1,42 @@
-namespace Coin-Combo
+using System;
+
+namespace CoinCombo
 {
-public class Coin-Combo
+  public class CoinCombo
   {
-  public bool ExampleTest(int year)
+    int remainder = 0;
+    int quarters = 0;
+    int dimes = 0;
+    int nickels = 0;
+    int pennies = 0;
+    public CoinCombo()
     {
-      // logic will go here
-      return false;
+
+    }
+    public int Coins(int number)
+    {
+
+      newNumber = New number()
+      {
+       quarters += number/25
+       remainder = %25
+       dimes+= number/10
+       remainder = %10
+       nickles+= number/5
+       remainder = %5
+       pennies += number/1
+      }
+    }
+  }
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("How much change do you have?");
+      int number = Console.ReadLine();
+      CoinCombo myCoins = new CoinCombo(number);
+      string result = quarters + " " + "Quarters" + dimes + " " + "Dimes" + nickels + " " + "Nickels" + pennies + " " + "Pennies";
+      Console.WriteLine(result);
     }
   }
 }
-  
---
+++ README.md
@@ -0,0 +1,37 @@
+# _Coin-Combo_
+
+#### _A app that sorts coins. June 27, 2018_
+
+#### By _**Jean Jia and Colin Bates**_
+
+## Description
+
+_This website was created to sort coins.
+Specs:
+1. accept user input in cents
+2. sort number of each coin needed
+3. return the smallest number of quarters, dimes, nickels, and pennies needed to make that change
+_
+
+## Setup/Installation Requirements
+
+* _Double click the icon in your folder_
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and contact details
+
+_Jean Jia email: Jean84646@gmail.com_
+_Colin Bates email: dragonflylord@gmail.com_
+
+## Technologies Used
+
+_Created with Atom editor.  Use any browser to view._
+
+### License
+
+*No license required*
+
+Copyright (c) 2018 **_Jean Jia and Colin Bates_**
